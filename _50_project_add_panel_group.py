761d847609251ba973c28f4d3d6eb6ff11ffec0a
uday bhaskar <gotouday@gmail.com>
2014-10-17 19:07:59 +0530

    moved these files into the gbp_ui directory
    Change-Id: I03b5b84761ec0c067e2c9ac11cd94bcf7e86cbde

--- _50_project_add_panel_group.py
+++
@@ -1,4 +0,0 @@
-ADD_INSTALLED_APPS = ['gbp_ui',]
-PANEL_GROUP = 'panel_group.GroupPolicyPanels'
-PANEL_GROUP_NAME = 'Group Policy'
-PANEL_GROUP_DASHBOARD = 'project'
--- _61_project_add_panel.py
+++
@@ -1,4 +0,0 @@
-PANEL = 'endpoint_groups_panel'
-PANEL_DASHBOARD = 'project'
-PANEL_GROUP = 'panel_group.GroupPolicyPanels'
-ADD_PANEL = 'gbp_ui.panels.contracts.panel.Contract' 
