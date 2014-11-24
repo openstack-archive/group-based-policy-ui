e2967c2885ca533d15ec448a71416448c9011ddd
uday bhaskar <gotouday@gmail.com>
2014-11-24 12:51:15 +0530

    GBP UI - added application policy UI
    Change-Id: I56879f4b97f24b3449ce2bf849d4236facf6da4d

---
+++ gbpui/column_filters.py
@@ -0,0 +1,132 @@
+# Copyright 2010-2011 OpenStack Foundation
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
+
+from gbpui import client
+
+LOG = logging.getLogger(__name__)
+
+
+def list_column_filter(items):
+    if len(items) == 0:
+        return ""
+    return items
+
+
+def update_pruleset_attributes(request, prset):
+    rules = prset.policy_rules
+    url = "horizon:project:application_policy:policyruledetails"
+    value = ["<ul>"]
+    li = lambda x: "<li><a href='" + \
+        reverse(url, kwargs={'policyrule_id': x.id}) + \
+        "'>" + x.name + "</a></li>"
+    for rule in rules:
+        r = client.policyrule_get(request, rule)
+        value.append(li(r))
+    value.append("</ul>")
+    value = "".join(value)
+    setattr(prset, 'policy_rules', mark_safe(value))
+    return prset
+
+
+def update_policy_target_attributes(request, pt):
+    url = "horizon:project:application_policy:policy_rule_set_details"
+    provided = pt.provided_policy_rule_sets
+    consumed = pt.consumed_policy_rule_sets
+    provided = [client.policy_rule_set_get(request, item) for item in provided]
+    consumed = [client.policy_rule_set_get(request, item) for item in consumed]
+    p = ["<ul>"]
+    li = lambda x: "<li><a href='" + \
+        reverse(url, kwargs={'policy_rule_set_id': x.id}) + \
+        "'>" + x.name + "</a></li>"
+    for item in provided:
+        p.append(li(item))
+    p.append("</ul>")
+    p = "".join(p)
+    c = ["<ul>"]
+    for item in consumed:
+        c.append(li(item))
+    c.append("</ul>")
+    c = "".join(c)
+    consumed = [item.name for item in consumed]
+    setattr(pt, 'provided_policy_rule_sets', mark_safe(p))
+    setattr(pt, 'consumed_policy_rule_sets', mark_safe(c))
+    l2url = "horizon:project:network_policy:l2policy_details"
+    if pt.l2_policy_id is not None:
+        policy = client.l2policy_get(request, pt.l2_policy_id)
+        u = reverse(l2url, {'l2policy_id': policy.id})
+        atag = mark_safe(
+            "<a href='" + u + "'>" + policy.name + "</a>")
+        setattr(pt, 'l2_policy_id', atag)
+    return pt
+
+
+def update_policyrule_attributes(request, prule):
+    url = "horizon:project:application_policy:policyclassifierdetails"
+    classifier_id = prule.policy_classifier_id
+    classifier = client. policyclassifier_get(request, classifier_id)
+    u = reverse(url, kwargs={'policyclassifier_id': classifier.id})
+    tag = mark_safe("<a href='" + u + "'>" + classifier.name + "</a>")
+    setattr(prule, 'policy_classifier_id', tag)
+    return prule
+
+
+def update_sc_spec_attributes(request, scspec):
+    nodes = scspec.nodes
+    nodes = [client.get_servicechain_node(request, item) for item in nodes]
+    value = ["<table class='table table-condensed'> \
+        <tr><td><span class='glyphicon glyphicon-remove-circle'>< /span></td>"]
+    for n in nodes:
+        value.append(
+            "<td><span class='glyphicon glyphicon-arrow-right'></span></td>")
+        value.append("<td>" + n.name + "(" + n.service_type + ")</td>")
+    value.append("</tr></table>")
+    setattr(scspec, 'nodes', mark_safe("".join(value)))
+    return scspec
+
+
+def update_sc_instance_attributes(request, scinstance):
+    ptg_url = "horizon:project:policytargets:policy_targetdetails"
+    clsurl = "horizon:project:application_policy:policyclassifierdetails"
+    scspec_url = "horizon:project:network_services:sc_spec_details"
+    consumer_ptg = scinstance.consumer_ptg
+    provider_ptg = scinstance.provider_ptg
+    scspec = scinstance.servicechain_spec
+    classifier = scinstance.classifier
+    if consumer_ptg is not None:
+        ptg = client.policy_target_get(request, consumer_ptg)
+        u = reverse(ptg_url, kwargs={'policy_target_id': ptg.id})
+        atag = "<a href='%s'>%s</a>" % (u, ptg.name)
+        setattr(scinstance, 'consumer_ptg', mark_safe(atag))
+    if provider_ptg is not None:
+        ptg = client.policy_target_get(request, consumer_ptg)
+        u = reverse(ptg_url, kwargs={'policy_target_id': ptg.id})
+        atag = "<a href='%s'>%s</a>" % (u, ptg.name)
+        setattr(scinstance, 'provider_ptg', mark_safe(atag))
+    if classifier is not None:
+        cls = client.policyclassifier_get(request, classifier)
+        u = reverse(clsurl, kwargs={'policyclassifier_id': cls.id})
+        atag = "<a href='%s'>%s</a>" % (u, cls.name)
+        setattr(scinstance, 'classifier', mark_safe(atag))
+    if scspec is not None:
+        sc = client.get_servicechain_spec(request, scspec)
+        u = reverse(scspec_url, kwargs={'scspec_id': sc.id})
+        atag = "<a href='%s'>%s</a>" % (u, sc.name)
+        setattr(scinstance, 'servicechain_spec', mark_safe(atag))
+    return scinstance
---
+++ gbpui/panels/application_policy/views.py
@@ -0,0 +1,199 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import re
+
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from horizon import exceptions
+from horizon import forms
+from horizon import messages
+from horizon import tabs
+from horizon import workflows
+
+from gbpui import client
+
+import forms as policy_rule_set_forms
+import tabs as policy_rule_set_tabs
+import workflows as policy_rule_set_workflows
+
+PolicyRuleSetTabs = policy_rule_set_tabs.ApplicationPoliciesTabs
+PolicyRuleSetDetailsTabs = policy_rule_set_tabs.ContractDetailsTabs
+PolicyRuleDetailsTabs = policy_rule_set_tabs.PolicyRuleDetailsTabs
+PolicyClassifierDetailsTabs = policy_rule_set_tabs.PolicyClassifierDetailsTabs
+PolicyActionDetailsTabs = policy_rule_set_tabs.PolicyActionDetailsTabs
+AddPolicyRuleSet = policy_rule_set_workflows.AddContract
+AddPolicyRule = policy_rule_set_workflows.AddPolicyRule
+AddPolicyClassifier = policy_rule_set_workflows.AddPolicyClassifier
+
+
+class IndexView(tabs.TabView):
+    tab_group_class = (PolicyRuleSetTabs)
+    template_name = 'project/application_policy/details_tabs.html'
+
+    def post(self, request, *args, **kwargs):
+        obj_ids = request.POST.getlist('object_ids')
+        action = request.POST['action']
+        obj_type = re.search('.delete([a-z]+)', action).group(1)
+        if not obj_ids:
+            obj_ids.append(re.search('([0-9a-z-]+)$', action).group(1))
+        if obj_type == 'policyaction':
+            for obj_id in obj_ids:
+                try:
+                    client.policyaction_delete(request, obj_id)
+                    messages.success(request, _('Deleted action %s') % obj_id)
+                except Exception as e:
+                    exceptions.handle(request,
+                                      _('Unable to delete action. %s') % e)
+        if obj_type == 'policyclassifier':
+            for obj_id in obj_ids:
+                try:
+                    client.policyclassifier_delete(request, obj_id)
+                    messages.success(
+                        request, _('Deleted classifer %s') % obj_id)
+                except Exception as e:
+                    exceptions.handle(request,
+                                      _('Unable to delete classifier. %s') % e)
+        if obj_type == 'policyrule':
+            for obj_id in obj_ids:
+                try:
+                    client.policyrule_delete(request, obj_id)
+                    messages.success(request,
+                                     _('Deleted rule %s') % obj_id)
+                except Exception as e:
+                    exceptions.handle(request,
+                                      _('Unable to delete rule. %s') % e)
+        if obj_type == 'policy_rule_set':
+            for obj_id in obj_ids:
+                try:
+                    client.policy_rule_set_delete(request, obj_id)
+                    messages.success(request,
+                                     _('Deleted rule %s') % obj_id)
+                except Exception as e:
+                    exceptions.handle(request,
+                            _('Unabled to delete policy_rule_set. %s') % e)
+
+        return self.get(request, *args, **kwargs)
+
+
+class AddPolicyRuleSetView(workflows.WorkflowView):
+    workflow_class = AddPolicyRuleSet
+    template_name = "project/application_policy/addpolicy_rule_set.html"
+
+    def get_object_id(self, policy_rule_set):
+        return [policy_rule_set.id]
+
+
+class UpdatePolicyRuleSetView(forms.ModalFormView):
+    form_class = policy_rule_set_forms.UpdatePolicyRuleSetForm
+    template_name = 'project/application_policy/update_policy_rule_set.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(
+            UpdatePolicyRuleSetView, self).get_context_data(**kwargs)
+        context['policy_rule_set_id'] = self.kwargs['policy_rule_set_id']
+        return context
+
+    def get_initial(self):
+        return {'policy_rule_set_id': self.kwargs['policy_rule_set_id']}
+
+
+class AddPolicyRuleView(workflows.WorkflowView):
+    workflow_class = AddPolicyRule
+    template_name = "project/application_policy/addpolicyrule.html"
+
+    def get_object_id(self, rule):
+        return [rule.id]
+
+
+class AddPolicyClassifierView(forms.ModalFormView):
+    form_class = policy_rule_set_forms.AddPolicyClassifierForm
+    template_name = "project/application_policy/add_policy_classifier.html"
+
+    def get_success_url(self):
+        return reverse('horizon:project:application_policy:index')
+
+    def get_object_id(self, classifier):
+        return [classifier.id]
+
+
+class AddPolicyActionView(forms.ModalFormView):
+    form_class = policy_rule_set_forms.AddPolicyActionForm
+    template_name = "project/application_policy/add_policy_action.html"
+
+    def get_success_url(self):
+        return reverse('horizon:project:application_policy:index')
+
+    def get_object_id(self, policyaction):
+        return [policyaction.id]
+
+
+class UpdatePolicyActionView(forms.ModalFormView):
+    form_class = policy_rule_set_forms.UpdatePolicyActionForm
+    template_name = "project/application_policy/update_policy_action.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(
+            UpdatePolicyActionView, self).get_context_data(**kwargs)
+        context['policyaction_id'] = self.kwargs['policyaction_id']
+        return context
+
+    def get_initial(self):
+        return {'policyaction_id': self.kwargs['policyaction_id']}
+
+
+class PolicyRuleSetDetailsView(tabs.TabView):
+    tab_group_class = (PolicyRuleSetDetailsTabs)
+    template_name = 'project/application_policy/details_tabs.html'
+
+
+class PolicyRuleDetailsView(tabs.TabView):
+    tab_group_class = (PolicyRuleDetailsTabs)
+    template_name = 'project/application_policy/details_tabs.html'
+
+
+class UpdatePolicyRuleView(forms.ModalFormView):
+    form_class = policy_rule_set_forms.UpdatePolicyRuleForm
+    template_name = "project/application_policy/update_policy_rule.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(UpdatePolicyRuleView, self).get_context_data(**kwargs)
+        context['policyrule_id'] = self.kwargs['policyrule_id']
+        return context
+
+    def get_initial(self):
+        return {'policyrule_id': self.kwargs['policyrule_id']}
+
+
+class PolicyClassifierDetailsView(tabs.TabView):
+    tab_group_class = (PolicyClassifierDetailsTabs)
+    template_name = 'project/application_policy/details_tabs.html'
+
+
+class UpdatePolicyClassifierView(forms.ModalFormView):
+    form_class = policy_rule_set_forms.UpdatePolicyClassifierForm
+    template_name = "project/application_policy/update_policy_classifier.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(
+            UpdatePolicyClassifierView, self).get_context_data(**kwargs)
+        context['policyclassifier_id'] = self.kwargs['policyclassifier_id']
+        return context
+
+    def get_initial(self):
+        return {'policyclassifier_id': self.kwargs['policyclassifier_id']}
+
+
+class PolicyActionDetailsView(tabs.TabView):
+    tab_group_class = (PolicyActionDetailsTabs)
+    template_name = 'project/application_policy/details_tabs.html'
