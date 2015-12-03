e2be9230f94b0efe55b0dc39208e6981b72ae2cc
ank <ank.svu@gmail.com>
2015-12-03 18:24:45 +0530

    Classifier to support custom IP protocol numbers
    Change-Id: I7cb461bae0908844f4456ef7048afa319a1dfe80
    Closes-Bug: #1499916
    (cherry picked from commit 75f6015af3b80093c572dec5d82efd7a293a8561)
    (cherry picked from commit ec39f5fae1c46b5214525440109d805c80a83719)

--- gbpui/fields.py
+++ gbpui/fields.py
@@ -11,8 +11,10 @@
 # under the License.
 
 from django.core import urlresolvers
+from django import forms
 from django.forms import fields
 from django.forms import widgets
+from django.utils.safestring import mark_safe
 
 
 class DynamicMultiSelectWidget(widgets.SelectMultiple):
@@ -65,3 +67,20 @@ class DynamicMultiChoiceField(fields.MultipleChoiceField):
 class CustomMultiChoiceField(DynamicMultiChoiceField):
     def validate(self, *args, **kwargs):
         return True
+
+
+class DropdownEditWidget(forms.TextInput):
+    def __init__(self, data_list, name, *args, **kwargs):
+        super(DropdownEditWidget, self).__init__(*args, **kwargs)
+        self._name = name
+        self._list = data_list
+        self.attrs.update({'list': 'list__%s' % self._name})
+
+    def render(self, name, value, attrs=None):
+        text_html = super(DropdownEditWidget, self).render(
+            name, value, attrs=attrs)
+        data_list = '<datalist id="list__%s">' % self._name
+        for item in self._list:
+            data_list += '<option value="%s">' % item
+        data_list += '</datalist>'
+        return mark_safe(text_html + data_list)
